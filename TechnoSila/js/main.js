6719a7bf2731a6d42913f756535254db616a27f6
LytkinaOlga <65194987+LytkinaOlga@users.noreply.github.com>
2020-05-13 17:47:52 +0300

    map and registration

--- TechnoSila/js/main.js
+++ TechnoSila/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
 
 });
+/*
+menu.onclick = function myFunction(){
+    var x = document.getElementById('myTopnav');
+
+    if (x.className ==="nav__link"){
+        x.className += " responsive";
+    } else {
+        x.className="nav__link";
+    }
+}*/
